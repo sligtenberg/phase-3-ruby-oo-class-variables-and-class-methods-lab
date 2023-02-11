5154ef0029ece2a7902424e3c08934d4b9bdd881
stephen ligtenberg <bountyhunter24@gmail.com>
2023-02-11 15:10:35 -0500

    commit tree 3c3cb1567abcef27d33e5df8423ccff25fc4d5ae

---
+++ lib/song.rb
@@ -0,0 +1,57 @@
+require 'pry'
+
+class Song
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    attr_accessor :name
+    attr_accessor :artist
+    attr_accessor :genre
+
+    def self.count 
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artist_count
+        count = {}
+        @@artists.each do |artist|
+            #binding.pry
+            if count[artist]
+                count[artist] += 1
+            else
+                count[artist] = 1
+            end
+        end
+        count
+    end
+
+    def self.genre_count
+        count = {}
+        @@genres.each do |genre|
+            if count[genre]
+                count[genre] += 1
+            else
+                count[genre] = 1
+            end
+        end
+        count
+    end
+end
